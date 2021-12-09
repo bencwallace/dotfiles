7be1b6b9b2a88b622534afc7a86e2dd0fd522aed
Ben Wallace <bencwallace@gmail.com>
2021-12-08 20:14:58 -0500

    force symlink creation

--- setup.sh
+++ setup.sh
@@ -2,35 +2,35 @@
 
 echo "Setting up i3"
 mkdir -p ~/.config/i3
-ln -s ${PWD}/i3/config ~/.config/i3/config
-ln -s ${PWD}/i3/adjust_brightness.sh ~/.config/i3/adjust_brightness.sh
-ln -s ${PWD}/i3/wallpaper.jpg ~/.config/i3/wallpaper.jpg
-ln -s ${PWD}/i3/lock.jpg ~/.config/i3/lock.jpg
+ln -sf ${PWD}/i3/config ~/.config/i3/config
+ln -sf ${PWD}/i3/adjust_brightness.sh ~/.config/i3/adjust_brightness.sh
+ln -sf ${PWD}/i3/wallpaper.jpg ~/.config/i3/wallpaper.jpg
+ln -sf ${PWD}/i3/lock.jpg ~/.config/i3/lock.jpg
 
 echo "Setting up i3status"
 mkdir -p ~/.config/i3status
-ln -s ${PWD}/i3status/config ~/.config/i3status/config
+ln -sf ${PWD}/i3status/config ~/.config/i3status/config
 
 echo "Setting up picom"
 mkdir -p ~/.config/picom
-ln -s ${PWD}/picom/picom.conf ~/.config/picom/picom.conf
+ln -sf ${PWD}/picom/picom.conf ~/.config/picom/picom.conf
 
 echo "Setting up polybar"
 mkdir -p ~/.config/polybar
-ln -s ${PWD}/polybar/config ~/.config/polybar/config
-ln -s ${PWD}/polybar/launch.sh ~/.config/polybar/launch.sh
+ln -sf ${PWD}/polybar/config ~/.config/polybar/config
+ln -sf ${PWD}/polybar/launch.sh ~/.config/polybar/launch.sh
 
 echo "Setting up rofi"
 mkdir -p ~/.config/rofi
-ln -s ${PWD}/rofi/config ~/.config/rofi/config
-ln -s ${PWD}/rofi/config.rasi ~/.config/rofi/config.rasi
-ln -s ${PWD}/rofi/dracula.rasi ~/.config/rofi/dracula.rasi
+ln -sf ${PWD}/rofi/config ~/.config/rofi/config
+ln -sf ${PWD}/rofi/config.rasi ~/.config/rofi/config.rasi
+ln -sf ${PWD}/rofi/dracula.rasi ~/.config/rofi/dracula.rasi
 
 echo "Setting up zsh"
-ln -s ${PWD}/zsh/.zshrc ~/.zshrc
+ln -sf ${PWD}/zsh/.zshrc ~/.zshrc
 
 echo "Setting up background"
-ln -s ${PWD}/wallpaper.jpg ~/wallpaper.jpg
+ln -sf ${PWD}/wallpaper.jpg ~/wallpaper.jpg
 
 echo "Setting up vim"
-ln -s ${PWD}/vim/.vimrc ~/.vimrc
+ln -sf ${PWD}/vim/.vimrc ~/.vimrc
